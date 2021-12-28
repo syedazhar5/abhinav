5b98da3bd5729f57f56b46fd7a7a19b5e2e5f0d2
syed azhar <syed.azhar9465@gmail.com>
2021-12-28 17:39:13 +0530

    project init

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+
+
+function getConnection()
+{
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "abhinavskin";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  return $conn;
+}
---
+++ process.php
@@ -0,0 +1,23 @@
+<?php
+include_once('connect.php');
+
+$conn = getConnection();
+
+// create a variable  
+$p_name=$_POST['p_name'];
+$contact_no =$_POST['contact_no'];
+$city=$_POST['city'];
+$appointment_date=$_POST['appointment_date'];
+$opd=$_POST['opd'];
+
+
+$sql = "INSERT INTO appointment_form(p_name, contact_no,city, appointment_date,opd) VALUES ('$p_name', '$contact_no','$city','$appointment_date','$opd')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Apppointment booked successfully";
+  header('refresh:5;url=index.html');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+?> 
