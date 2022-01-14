7fc153a8d4e6cf7926095b7d7e5fc1d51b3aaa3d
syed azhar <syed.azhar9465@gmail.com>
2022-01-14 17:45:53 +0530

    Paragrapg adjustment

--- earandnose.php
+++ earandnose.php
@@ -1,20 +1,7 @@
 <?php include_once('templates/header.php') ?>
-<div class="row">
-            <div class="col-lg-4">
-                <img src="images\ear.jpg" alt="" width="300" style="margin: 20px;">
-                <img src="images\nose.jpeg" alt="" width="300" style="margin: 20px;">
-            </div>
-            <div class="col-lg-8">
-                <div class="font-weight-bold">
-                    <p>
-                        Ear and Nose Piercing This procedure has been done ever since human beings started wearing jewellery. However from the point of view of safety against infections that such a procedure may give rise to; it is advisable to do this procedure in a controlled
-                        and sterile environment of a medical clinic. we use the best ear and nose piercing system. This system ensures hygiene, accuracy of the piercing and minimum trauma. At the same time we do not compromise on the aesthetics of the
-                        process. A wide variety of safe ear studs are available to choose from ensuring aesthetics even post procedure. Also there are a wide variety of ear studs meant specifically for children. Which help put little ones at ease. ear
-                        and nose piercing system The Procedure: The procedure involves first numbing out the area to be pierced or punctured with a numbing cream. Following this a trained doctor marks the exact point where the puncture of piercing is
-                        required. This is always confirmed with the patient or the guardian. This may be followed my minor post procedure care instructions as prescribed by the doctor. INURSKN in Powai, Mumbai is a preferred destination for the purpose
-                        of piercing as we provide the sterile environment of a clinic while being far more affordable than nearby hospitals. Here are some dos and do nots for post procedure care:
-                    </p>
-                </div>
-            </div>
-        </div>
- <?php include_once('templates/footer.php') ?>
+<center><img src="images\ear.jpg" alt="" width="300" style="margin: 20px;">
+<img src="images\nose.jpeg" alt="" width="300" style="margin: 20px;"></center>
+                <p>
+                 <b>Ear and Nose Piercing - </b> This procedure has been done ever since human beings started wearing jewellery. However from the point of view of safety against infections that such a procedure may give rise to, it is advisable to do this procedure in a controlled and sterile environment of a medical clinic. We use the best ear and nose piercing system. This system ensures hygiene, accuracy of the piercing and minimum trauma. At the same time we do not compromise on the aesthetics of the process. A wide variety of safe ear studs are available to choose from ensuring aesthetics even post procedure. Also there are a wide variety of ear studs meant specifically for children. Which help put little ones at ease. Ear and nose piercing system - the Procedure: The procedure involves first numbing out the area to be pierced or punctured with a numbing cream. Following this a trained doctor marks the exact point where the puncture of piercing is required. This is always confirmed with the patient or the guardian. This may be followed my minor post procedure care instructions as prescribed by the doctor. We provide a better destination for the purpose of piercing as we provide the sterile environment of a clinic while being far more affordable than nearby hospitals.
+                 </p>
+            <?php include_once('templates/footer.php') ?>
